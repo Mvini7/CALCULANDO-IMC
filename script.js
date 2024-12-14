cc3f5b18eefa8fa5d048043258e9d6f127d13296
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2024-12-14 11:03:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const calcular = document.getElementById("calcular")
+const resultado = document.getElementById("resultado")
+
+
+function calcularIMC() {
+    const nome = document.querySelector("#nome").value
+    const peso = document.querySelector("#peso").value
+    const altura = document.querySelector("#altura").value
+    const calculo = (peso / (altura ** 2)).toFixed(2)
+    const expondoResultado = resultado.textContent = `${nome}, seu IMC é: ${calculo}!`
+
+    if (nome !== '') {
+        
+    } else {
+        alert("Nenhum campo pode estar vazio.")
+    }
+}
+
+
+calcular.addEventListener('click', calcularIMC)
