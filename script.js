892a5f52ce7fb1e61f1dece2952dd825beae270e
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2024-12-16 17:45:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function calcularIMC() {
     const nome = document.querySelector("#nome").value
     const peso = document.querySelector("#peso").value
     const altura = document.querySelector("#altura").value
-    const calculo = (peso / (altura ** 2)).toFixed(2)
+    const calculo = (peso / (altura * 2)).toFixed(2)
     const expondoResultado = resultado.textContent = `${nome}, seu IMC é: ${calculo}!`
 
     if (nome !== '') {
